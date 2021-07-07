ab76bb5f7533adc024e70068f045a57edccf363f
Julia <lilu.just@mail.ru>
2021-07-07 23:53:39 +0300

    added ghost class

---
+++ ghost.cpp
@@ -0,0 +1,13 @@
+#include "ghost.h"
+
+ghost::ghost()
+{
+
+}
+
+ghost::ghost(int x, int y)
+{
+    ghostPos.first=x;
+    ghostPos.second=y;
+}
+
---
+++ ghost.h
@@ -0,0 +1,14 @@
+#ifndef GHOST_H
+#define GHOST_H
+#include <vector>
+
+class ghost
+{
+public:
+    ghost();
+    ghost(int, int);
+
+   std::pair<int,int> ghostPos;
+};
+
+#endif // GHOST_H
